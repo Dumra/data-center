7cbd6293724aa96ce24c886cd5e450cad8627848
Dumra <d.shuliakov@gmail.com>
2016-01-27 01:20:34 +0200

    Fixed some bugs at home

--- database/seeds/TaskValuesTableSeeder.php
+++ database/seeds/TaskValuesTableSeeder.php
@@ -11,7 +11,7 @@ class TaskValuesTableSeeder extends Seeder
             'longitude' => 35.20,
             'height' => 4,
             'direction' => 'N',
-            'task_id' => 1
+            'task_id' => 4
         ]);
 
        factory(App\Data\Models\TaskValue::class)->create([
@@ -19,7 +19,7 @@ class TaskValuesTableSeeder extends Seeder
             'longitude' => 42.20,
             'height' => 0,
             'direction' => 'S',
-            'task_id' => 1
+            'task_id' => 4
         ]);
 
         factory(App\Data\Models\TaskValue::class)->create([
@@ -27,7 +27,7 @@ class TaskValuesTableSeeder extends Seeder
             'longitude' => 93.25,
             'height' => 8,
             'direction' => 'W',
-            'task_id' => 2
+            'task_id' => 5
         ]);
 
         factory(App\Data\Models\TaskValue::class)->create([
@@ -35,7 +35,7 @@ class TaskValuesTableSeeder extends Seeder
             'longitude' => 34.25,
             'height' => 15,
             'direction' => 'N',
-            'task_id' => 3
+            'task_id' => 6
         ]);
 
 
